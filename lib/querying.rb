1c45d2530af1f5117f48598791adc505f427b180
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 09:24:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title,year from books group by books.series_id order by year asc"
+  "SELECT series_id from series order by id asc limit 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
