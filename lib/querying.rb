428aa168ca0d4e0e0681635c5be0fbe5bd31064a
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 06:29:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name,subgenres.name from authors inner join series on series.series_id = authors.series_id INNER JOIN subgenres on subgenres.id = series.subgenre_id"
+  "SELECT authors.name,subgenres.name from authors inner join series on series.id = authors.series_id INNER JOIN subgenres on subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
